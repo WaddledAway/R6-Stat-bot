9c88c31e258808f7e24ee046c47714804e1043aa
WaddledAway <45279779+WaddledAway@users.noreply.github.com>
2020-04-07 13:23:07 -0500

    Add files via upload

---
+++ r6_stat_bot/r6_stat_bot/bot.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+import sys, traceback
+
+prefix = '!'
+
+bot = commands.Bot(command_prefix=prefix, description='R6 Stats Bot')
+bot.remove_command('help')
+
+extensions = ['commands']
+
+if __name__ == '__main__':
+    for extension in extensions:
+        try:
+            bot.load_extension(extension)
+        except Exception as e:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
+
+@bot.event
+async def on_ready():
+    print(f'Ready as: {bot.user.name}')
+    game = discord.Game("Rainbow Six Siege")
+    await bot.change_presence(status=discord.Status.online, activity=game)
+
+bot.run(process.env.DISCORD_TOKEN, bot=True, reconnect=True) 
